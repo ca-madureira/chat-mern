21ae132be4d6fd6ba0d11a36125c12c0da1938b8
camila madureira <ca.madureira094@gmail.com>
2023-08-23 20:19:56 -0300

    feat: create Authentication APIs in nodejs

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/userRoute");
+
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/users", userRoute);
+
+app.get("/", (req, res) => {
+  res.send("Bem-vindo");
+});
+
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+app.listen(port, (req, res) => {
+  console.log(`Servidor rodando na porta: ${port}`);
+});
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("conexao mongodb estabelecida"))
+  .catch((error) => console.log("conexão mongodb falhou", error.message));
