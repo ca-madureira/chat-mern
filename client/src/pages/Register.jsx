9f0a9caa253491d6e6c85947a4ca0e4159c1249b
camila madureira <ca.madureira094@gmail.com>
2023-08-23 22:10:44 -0300

    feat: Apply bootstrap at login and register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,35 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+
 const Register = () => {
-  return <>Register</>;
+  return (
+    <>
+      <Form>
+        <Row
+          style={{
+            height: "100vh",
+            justifyContent: "center",
+            paddingTop: "10%",
+          }}
+        >
+          <Col sx={6}>
+            <Stack gap={3}>
+              <h2>Cadastro de usuário</h2>
+              <Form.Control type='text' placeholder='Name' />
+              <Form.Control type='email' placeholder='Email' />
+              <Form.Control type='password' placeholder='Password' />
+              <Button variant='primary' type='submit'>
+                Cadastrar
+              </Button>
+
+              <Alert variant='danger'>
+                <p>Um erro ocorreu</p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
 };
 
 export default Register;
